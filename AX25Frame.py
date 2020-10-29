6e3a9984caad5889798449eab463e4ed58edeb0b
or00101 <orkooki123@gmail.com>
2020-10-29 11:37:39 +0200

    Update AX25Frame.py

--- AX25Frame.py
+++ AX25Frame.py
@@ -17,7 +17,7 @@ class AX25Frame:
         for i in range(0, len(broadcast) - 8):
             #print(broadcast[i: i + 8])
             if self.is_flag(broadcast[i: i + 8]):
-                return i
+                return o
         return -1
 
     def get_all_flags_indecies(self, broadcast):
